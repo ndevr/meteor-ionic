6e916684703c3ad7d802e4f474c5b4ec0e5b1cf2
Jens Neumann <jens.neumann@spotflare.de>
2015-02-09 17:51:03 +0100

    fixed dependencies

--- package.js
+++ package.js
@@ -8,7 +8,6 @@ Package.on_use(function (api, where) {
   api.versionsFrom('METEOR@0.9.0.1');
 
   api.use('urigo:angular@0.6.5', 'client', {weak: true});
-  api.use('angularui:angular-ui-router@0.2.13', 'client');
 
   api.use([
     'fastclick@1.0.2'
@@ -16,15 +15,12 @@ Package.on_use(function (api, where) {
 
   // Fix icons to absolute path
   api.addFiles([
-    'dependencies/angular-animate.js',
-    'dependencies/angular-sanitize.js',
     'lib/css/ionic.css',
     'lib/fonts/ionicons.eot',
     'lib/fonts/ionicons.svg',
     'lib/fonts/ionicons.ttf',
     'lib/fonts/ionicons.woff',
-    'lib/js/ionic.js',
-    'lib/js/ionic-angular.js'
+    'lib/js/ionic.bundle.js',
   ], 'client');
 
   // Stop Meteor's Fastclick for Ionic one
